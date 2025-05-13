94e29b6bedd5cb16e4598f6268583eee7785f30b
Noedig <163137393+Noe-dig@users.noreply.github.com>
2025-05-13 12:15:54 +0200

    commit

---
+++ eersteproject/Program.cs
@@ -0,0 +1,8 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+int getal = 19;
+string karakterNaam = "Kratos";
+bool boolean = false;
+float getalFloat = 19.5f;
+
+Console.WriteLine(getal+" "+karakterNaam+" "+boolean+" "+getalFloat);
